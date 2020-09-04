36c17b6a107eb91bac16acbc162500dade11b8bf
Waldo Theron <ytk0657@autuni.ac.nz>
2020-09-04 14:42:00 +1200

    JUnit Framework
    Testing UI
    Testing Instruments

--- app/src/main/java/nz/co/theron/iptv/SetupActivity.java
+++ app/src/main/java/nz/co/theron/iptv/SetupActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.media.tv.TvInputInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -79,6 +80,78 @@ public class SetupActivity extends FragmentActivity {
                     .build();
 
             actions.add(action);
+        }
+
+        @Override
+        public void onGuidedActionClicked(GuidedAction action) {
+            // Move onto the next step
+            GuidedStepSupportFragment fragment = new AccountFragment();
+            fragment.setArguments(getArguments());
+            add(getFragmentManager(), fragment);
+        }
+    }
+
+    public static class AccountFragment extends GuidedStepSupportFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @NonNull
+        @Override
+        public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+
+            return new GuidanceStylist.Guidance(
+                    "View Accounts",
+                    "this is accounts",
+                    getString(R.string.account_label),
+                    ContextCompat.getDrawable(getActivity(), R.drawable.setup_logo2));
+        }
+
+        @Override
+        public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
+
+            List<GuidedAction> subActions = new ArrayList<GuidedAction>();
+            subActions.add(new GuidedAction.Builder(getActivity())
+                    .id(1)
+                    .title("action2")
+                    .description("desc")
+                    .build());
+
+            subActions.add(new GuidedAction.Builder(getActivity())
+                    .id(1)
+                    .title("action3")
+                    .description("desc")
+                    .build());
+
+
+
+            GuidedAction actionWithSubActions = new GuidedAction.Builder(getActivity())
+                    .title("Button")
+                    .description("Button action")
+                    .editable(false)
+                    .subActions(subActions)
+                    .build();
+
+            GuidedAction action2 = new GuidedAction.Builder(getActivity())
+                    .title("Button")
+                    .description("Button action")
+                    .editable(false)
+                    .build();
+
+            actions.add(actionWithSubActions);
+            actions.add(action2);
+        }
+
+        @Override
+        public boolean onSubGuidedActionClicked(GuidedAction action) {
+            // Check for which action was clicked, and handle as needed
+            if (action.getId() == 1) {
+                Toast.makeText(getActivity(), "You clicked action2", Toast.LENGTH_SHORT).show();
+            }
+            // Return true to collapse the subactions drop-down list, or
+            // false to keep the drop-down list expanded.
+            return true;
         }
 
         @Override
