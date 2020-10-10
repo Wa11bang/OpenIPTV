df19c2e05e9e8e392fd6ab4a62f4f9d61fc3d92f
daysts232 <daysts232@gmail.com>
2020-10-10 13:41:25 +1300

    Listener Framework Added

--- app/src/main/java/com/openiptv/code/PreferenceActivity.java
+++ app/src/main/java/com/openiptv/code/PreferenceActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
 
 public class PreferenceActivity extends FragmentActivity {
     @Override
@@ -17,4 +21,24 @@ public class PreferenceActivity extends FragmentActivity {
                 .commit();
 
     }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                // Set the summary to reflect the new value
+                preference.setSummary(index > 0
+                        ? listPreference.getEntries()[index]
+                        : null);
+            } else if (preference instanceof EditTextPreference) {
+                preference.setSummary(stringValue);
+            } else if (preference instanceof SwitchPreference) {
+                preference.setSummary(stringValue);
+            }
+            return false;
+        }
+    };
 }
