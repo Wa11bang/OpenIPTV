42cd52e8162482f3db55fba9ded4d4b9f7af4b0c
daysts232 <daysts232@gmail.com>
2020-10-15 18:00:57 +1300

    Preferences Listener Finished

--- app/src/main/java/com/openiptv/code/PreferenceActivity.java
+++ app/src/main/java/com/openiptv/code/PreferenceActivity.java
@@ -1,5 +1,6 @@
 package com.openiptv.code;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -7,9 +8,14 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
 public class PreferenceActivity extends FragmentActivity {
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,25 +26,21 @@ public class PreferenceActivity extends FragmentActivity {
                 .replace(R.id.main_frame, new PreferenceFragment())
                 .commit();
 
+        /**
+         * Listener which performs functions when preferences are changed.
+         * @param sharedPreferences Preference that is changed
+         * @param key Preferences key
+         */
+        preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if (key.equals()){
+
+                }
+            }
+
+        };
+
     }
 
-    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            String stringValue = newValue.toString();
-            if (preference instanceof ListPreference) {
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-                // Set the summary to reflect the new value
-                preference.setSummary(index > 0
-                        ? listPreference.getEntries()[index]
-                        : null);
-            } else if (preference instanceof EditTextPreference) {
-                preference.setSummary(stringValue);
-            } else if (preference instanceof SwitchPreference) {
-                preference.setSummary(stringValue);
-            }
-            return false;
-        }
-    };
 }
